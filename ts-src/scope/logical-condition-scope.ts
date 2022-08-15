25ac09315258d02cdfdb8ee612efecbe8bf07f90
Franz Zemen <franzzemen@gmail.com>
2022-08-15 08:14:51 -0400

    ncu

--- ts-src/scope/logical-condition-scope.ts
+++ ts-src/scope/logical-condition-scope.ts
@@ -1,9 +1,10 @@
 import {ExecutionContextI} from '@franzzemen/app-utility';
+import {Scope} from '@franzzemen/re-common';
 import {ConditionScope} from '@franzzemen/re-condition';
 import {LogicalConditionOptions} from './logical-condition-options';
 
 export class LogicalConditionScope extends ConditionScope {
-  constructor(options?: LogicalConditionOptions, ec?: ExecutionContextI) {
-    super();
+  constructor(options?: LogicalConditionOptions, parentScope?: Scope, ec?: ExecutionContextI) {
+    super(options, parentScope, ec);
   }
 }
