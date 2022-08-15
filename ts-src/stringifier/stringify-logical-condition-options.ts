2a72b0a172a2a1d27fa6e21fb522f5b4fb6c2895
Franz Zemen <franzzemen@gmail.com>
2022-08-15 07:43:02 -0400

    ncu

---
+++ ts-src/stringifier/stringify-logical-condition-options.ts
@@ -0,0 +1,5 @@
+import {StringifyConditionOptions} from '@franzzemen/re-condition/stringifier/stringify-condition-options';
+
+export interface StringifyLogicalConditionOptions extends StringifyConditionOptions {
+  
+}
