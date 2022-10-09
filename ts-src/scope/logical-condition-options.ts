c36459ed03abc71c9387f930c2dbf82b2c5103fe
Franz Zemen <franzzemen@gmail.com>
2022-10-09 07:55:33 -0400

    mergeLogicalConditionOptions

--- ts-src/scope/logical-condition-options.ts
+++ ts-src/scope/logical-condition-options.ts
@@ -1,5 +1,9 @@
-import {ConditionOptions} from '@franzzemen/re-condition';
+import {_mergeConditionOptions, ConditionOptions} from '@franzzemen/re-condition';
 
 export interface LogicalConditionOptions extends ConditionOptions {
 
+}
+
+export function _mergeLogicalConditionOptions(source: LogicalConditionOptions, target: LogicalConditionOptions, mergeInto = true): LogicalConditionOptions {
+  return _mergeConditionOptions(source, target, mergeInto);
 }
